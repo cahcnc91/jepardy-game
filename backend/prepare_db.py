153c99a4a998998453f5d2e4045ea3a01d49abe1
NieroCoC <nierococ@mskcc.org>
2020-12-17 15:32:36 -0500

    fix: prepare_db account for db does not exists yet

--- backend/prepare_db.py
+++ backend/prepare_db.py
@@ -1,28 +1,6 @@
+from mysql.connector import errorcode
 import mysql.connector
 from datamock import lists
-
-print('Conectando...')
-
-mysql_config = {
-    'user': 'user',
-    'password': 'password',
-    'host': '127.0.0.1',
-    'port': '3306',
-    'database': 'jepardy'
-}
-
-
-conn = mysql.connector.connect(**mysql_config)
-if conn.is_connected():
-    print('connected to database')
-
-
-cursor = conn.cursor()
-
-# DROP EXISTING TABLE
-drop_table = "DROP TABLE questions;"
-cursor.execute(drop_table)
-conn.commit()
 
 # CREATE NEW TABLE
 create_table = '''
@@ -35,19 +13,76 @@ create_table = '''
         PRIMARY KEY (id)
     );'''
 
-cursor.execute(create_table)
-conn.commit()
+print('Conectando...')
 
-# INSERT QUESTIONS
-cursor.executemany(
-    'INSERT INTO questions (question, category, answer, difficulty) VALUES (%s, %s, %s, %s)', lists)
+mysql_config = {
+    'user': 'user',
+    'password': 'password',
+    'host': '127.0.0.1',
+    'port': '3306',
+}
 
-cursor.execute('select * from questions')
+conn = mysql.connector.connect(**mysql_config)
+if conn.is_connected():
+    print('connected to database')
 
-for question in cursor.fetchall():
-    print(question)
 
-conn.commit()
+cursor = conn.cursor()
+
+
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format("jepardy"))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+
+try:
+    cursor.execute("USE {}".format("jepardy"))
+except mysql.connector.Error as err:
+    print()
+    print("Database {} does not exists.".format("jepardy"))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print("Database {} created successfully.".format("jepardy"))
+        conn.database = "jepardy"
+    else:
+        print(err)
+        exit(1)
+
+# DROP EXISTING TABLE
+try:
+    print("Creating table {}: ".format("questions"), end='')
+    cursor.execute(create_table)
+
+        # INSERT QUESTIONS
+    cursor.executemany(
+        'INSERT INTO questions (question, category, answer, difficulty) VALUES (%s, %s, %s, %s)', lists)
+
+    cursor.execute('select * from questions')
+
+    for question in cursor.fetchall():
+        print(question)
+
+    conn.commit()
+    
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+        print("already exists.")
+    else:
+        print(err.msg)
+else:
+    print("OK")
+
 
 cursor.close()
 conn.close()
+
+# drop_table = "DROP TABLE questions;"
+# cursor.execute(drop_table)
+# conn.commit()
+
+# cursor.execute(create_table)
+# conn.commit()
